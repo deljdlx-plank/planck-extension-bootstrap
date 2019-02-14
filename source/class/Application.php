418351f6e793dfe3e46e7ac9efcd93ade7ab8ad5
Jean luc <you@example.com>
2019-02-14 09:00:55 +0100

    synchro

--- source/class/Application.php
+++ source/class/Application.php
@@ -3,6 +3,7 @@
 namespace Planck\Extension\Bootstrap;
 
 
+use Planck\Extension\Bootstrap\Router\Extension as ExtensionRouter;
 use Planck\ExtensionLoader;
 
 class Application extends \Planck\Application\Application
@@ -11,9 +12,17 @@ class Application extends \Planck\Application\Application
 
 
 
+    public function initialize()
+    {
+        parent::initialize();
 
+        $this->addRouter(
+            new ExtensionRouter(),
+            ExtensionRouter::class
+        );
 
-
+        return $this;
+    }
 
 
 }
